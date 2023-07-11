8eb5e21a81783502036b665a2e39202db30d1982
Jenniffer Melchiade <jdenisemelch@gmail.com>
2023-07-11 17:05:03 -0400

    JOptionPane method for diaolog input

--- TestCertification.java
+++ TestCertification.java
@@ -2,10 +2,11 @@ import javax.swing.JOptionPane;
 
 public class TestCertification {
     public static void main(String[] arg) {
-        int x = GetData.getInt("1. Life insurance\n 2. Health insurance\n 3. Mortgage\n 4. Appraisal\n Make selection");
+        String userInput = JOptionPane.showInputDialog(
+                "1. Life insurance\n 2. Health insurance\n 3. Mortgage\n 4. Appraisal\n Make selection");
+        int x = Integer.parseInt(userInput);
         Certification c = new Certification(x);
         c.getCost();
         System.out.println(c);
-
     }
 }
